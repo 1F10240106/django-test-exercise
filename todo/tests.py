ccc1f66170fc87efe75cb4202a3f351e584e07ee
1f10240106 <s1f102401067@iniad.org>
2025-07-10 00:07:05 +0900

    修正3

--- todo/tests.py
+++ todo/tests.py
@@ -3,24 +3,24 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
 
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        task = Task(title="task1", due_at=due)
+        due=timezone.make_aware(datetime(2024,6,30,23,59,59))
+        task=Task(title="task1",due_at=due)
         task.save()
-
-        task = Task.objects.get(pk=task.pk)
+        task=Task.objects.get(pk=task.pk)
         self.assertEqual(task.title,"task1")
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at,due)
-    
+
     def test_create_task2(self):
         task = Task(title="task2")
         task.save()
@@ -52,6 +52,7 @@ class TaskModelTestCase(TestCase):
         task.save()
 
 
+
         self.assertFalse(task.is_overdue(current))
 
 
@@ -68,7 +69,6 @@ class TodoViewTestCase(TestCase):
         client = Client()
         data={'title':'Test Task','due_at':'2024-06-30 23:59:59'}
         response=client.post('/',data)
-
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
@@ -80,8 +80,7 @@ class TodoViewTestCase(TestCase):
         task2.save()
         client = Client()
         response = client.get('/?order=post')
-
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,'todo/index.html')
         self.assertEqual(response.context['tasks'][0],task2)
         self.assertEqual(response.context['tasks'][1],task1)
@@ -98,3 +97,4 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+
--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-# Create your views here.
+
 
 def index(request):
     if request.method == 'POST':
@@ -20,3 +20,4 @@ def index(request):
         'tasks': tasks,
     }
     return render(request, 'todo/index.html', context)
+
